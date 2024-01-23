13150d78e28a51f61f7a7eb3eb5dceb636abc34b
Isaac1676 <ezechielncho225@gmail.com>
2024-01-23 02:01:56 +0000

    services

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,15 @@ import 'package:ace_app/pages/form.dart';
 import 'package:flutter/material.dart';
 import 'package:ace_app/components/bar_bottom.dart';
 import 'package:ace_app/pages/home.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async{
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
+
   runApp(const MyApp());
 }
 
